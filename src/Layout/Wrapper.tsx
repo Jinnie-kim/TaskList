cb9d354963aa49ed5a187341ac5b7118471624c2
Jinnie <hjk7639@gmail.com>
2023-03-09 16:56:01 +0900

    컴포넌트 공통 레이아웃 스타일 Wrapper 컴포넌트 생성

--- src/Layout/Wrapper.tsx
+++ src/Layout/Wrapper.tsx
@@ -1,13 +1,14 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import { PropsWithChildren } from 'react';
 
 const wrapperStyle = css`
   width: 900px;
   margin: 0 auto;
 `;
 
-const Wrapper = () => {
-  return <div css={wrapperStyle}></div>;
+const Wrapper = ({ children }: PropsWithChildren) => {
+  return <div css={wrapperStyle}>{children}</div>;
 };
 
 export default Wrapper;
