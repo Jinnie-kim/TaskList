c6a9139db4fcc4400d3c6eda2432e946de085767
Jinnie <hjk7639@gmail.com>
2023-03-09 16:14:13 +0900

    공통 레이아웃 Wrapper 컴포넌트 생성

---
+++ src/Layout/Wrapper.tsx
@@ -0,0 +1,13 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+const wrapperStyle = css`
+  width: 900px;
+  margin: 0 auto;
+`;
+
+const Wrapper = () => {
+  return <div css={wrapperStyle}></div>;
+};
+
+export default Wrapper;
