3f3d3b97cea7c8fe4ab89600bc5527c4762eaac4
Jinnie <hjk7639@gmail.com>
2023-03-09 16:11:25 +0900

    프로젝트 루트에 Global CSS 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Global } from '@emotion/react';
+import reset from './GlobalStyle';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <>
+      <Global styles={reset} />
+      <App />
+    </>
   </React.StrictMode>
 );
