fc865960436c1f598a5560d5949af55986bff052
Jinnie <hjk7639@gmail.com>
2023-03-10 20:17:12 +0900

    App에서 화면에 보여줄 컴포넌트들 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Header from './component/Header';
+import Login from './component/Login';
+import Join from './component/Join';
+import Task from './component/Task';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Task />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/join',
+    element: <Join />,
+  },
+]);
+
 function App() {
   return (
     <>
       <Header />
+      <RouterProvider router={router} />
     </>
   );
 }
